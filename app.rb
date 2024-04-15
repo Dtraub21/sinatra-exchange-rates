813107a180f9178a01ad5affe55c54db10fb107a
Dtraub21 <164813811+Dtraub21@users.noreply.github.com>
2024-04-15 11:42:23 +0000

    roadblock

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'http'
+require 'json'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Route for the homepage listing all currencies
+get '/' do
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV['EXCHANGE_RATE_KEY']}"
+  response = HTTP.get(api_url)
+  @currencies = JSON.parse(response.body.to_s)['symbols']
+  erb :index
+end
+
+# Route for a specific currency showing conversion links to all other currencies
+get '/:from_currency' do
+  @from_currency = params['from_currency'].upcase
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV['EXCHANGE_RATE_KEY']}"
+  response = HTTP.get(api_url)
+  @currencies = JSON.parse(response.body.to_s)['symbols']
+  erb :currency
+end
+
+# Route for converting from one specific currency to another
+get '/:from_currency/:to_currency' do
+  @from_currency = params['from_currency'].upcase
+  @to_currency = params['to_currency'].upcase
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV['EXCHANGE_RATE_KEY']}&from=#{@from_currency}&to=#{@to_currency}&amount=1"
+  response = HTTP.get(api_url)
+  @result = JSON.parse(response.body.to_s)['result']
+  erb :conversion
 end
